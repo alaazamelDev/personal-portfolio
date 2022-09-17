ea5668ef05022911fbb4f9cae29c4d6776940223
alaazamelDev <alaa.zamel23@gmail.com>
2022-09-17 21:19:13 +0300

    feat: add the official link to feedbacks

--- src/components/testimonials/FeedbackCard.jsx
+++ src/components/testimonials/FeedbackCard.jsx
@@ -6,14 +6,16 @@ import "./testimonials.css";
 const FeedbackCard = ({ feedback }) => {
   return (
     <div className="testimonial__card">
-      <img
-        src={feedback.image}
-        alt={feedback.title}
-        className="testimonial__card-img"
-      />
+      <a href={feedback.url} className="testimonial__card-link">
+        <img
+          src={feedback.image}
+          alt={feedback.title}
+          className="testimonial__card-img"
+        />
 
-      <h3 className="testimonial__card-name">{feedback.title}</h3>
-      <p className="testimonial__card-description">{feedback.description}</p>
+        <h3 className="testimonial__card-name">{feedback.title}</h3>
+        <p className="testimonial__card-description">{feedback.description}</p>
+      </a>
     </div>
   );
 };
--- src/models/testimonial.js
+++ src/models/testimonial.js
@@ -1,13 +1,13 @@
 // load avatar placeholder
-import avatar from "../assets/testimonial1.png";
-import avatar2 from "../assets/testimonial2.png";
+import avatar from "../assets/testimonial.png";
 
 export class Testimonial {
-  constructor({ id, image, title, description }) {
+  constructor({ id, image, title, description, url }) {
     this.id = id;
     this.image = image;
     this.title = title;
     this.description = description;
+    this.url = url;
   }
 }
 
@@ -18,13 +18,15 @@ export const feedbacks = [
     title: "Khalid Maliki",
     description:
       "Mr. Alaa is one of the best programmers who combine skill, honesty and price. I would recommend dealing with him without hesitation",
+    url: "https://mostaql.com/u/alaazamel/reviews/4326219",
   }),
   new Testimonial({
     id: 2,
-    image: avatar2,
+    image: avatar,
     title: "Mona Saad",
     description:
       "Speed and accuracy in completing the project efficiently and professionally, I highly recommend dealing with him.",
+    url: "https://mostaql.com/u/alaazamel/reviews/4175447",
   }),
   new Testimonial({
     id: 3,
@@ -32,5 +34,6 @@ export const feedbacks = [
     title: "Khalid Maliki",
     description:
       "The best programmer I've dealt with gets the job done in the shortest period of time and the best quality Classy handling and courteous work I wish him success",
+    url: "https://mostaql.com/u/alaazamel/reviews/4272890",
   }),
 ];
